1e56aa2954dc7e232c74da6ec4b32440a4703268
olegrog <olegrog@52f9c578-8a65-4b12-949d-9b6adbe03df8>
2013-02-01 07:50:11 +0000

    new data
    git-svn-id: https://localhost/svn/reports@2 52f9c578-8a65-4b12-949d-9b6adbe03df8

--- 1_convergence_heat/plot.py
+++ 1_convergence_heat/plot.py
@@ -10,23 +10,28 @@ params = {'backend': 'pdf',
           'text.usetex': True,
           'figure.figsize': [5,4]}
 py.rcParams.update(params)
+import numpy as np
 
 kappa = 2.129475
 w=2
 data = py.loadtxt("data.txt")
-r,old,s1,new,s2 = data.T
+r,old,s1,new,s2,exper,s3 = data.T
 
-py.plot(r, abs(new-kappa), color='b', linewidth=w, label=r'$\mathrm{new}$')
-py.errorbar(r, abs(new-kappa), yerr=s2, color='b')
-py.plot(r, abs(old-kappa), color='r', linewidth=w, label=r'$\mathrm{old}$')
+py.plot(r, (new-kappa), color='b', linewidth=w, label=r'$\mathrm{new}$')
+py.errorbar(r, (new-kappa), yerr=s2, color='b')
+py.plot(r, old-kappa, color='r', linewidth=w, label=r'$\mathrm{old}$')
 py.errorbar(r, old-kappa, yerr=s1, color='r')
+
+exper = np.ma.masked_where(exper == 0, exper)
+py.plot(r, exper-kappa, color='g', linewidth=w, label=r'$\mathrm{exper}$')
+py.errorbar(r, exper-kappa, yerr=s3, color='g')
 
 py.axhline(color='black')
 py.xlabel(r'$N_R$')
 py.ylabel(r'$\lambda-\lambda_\mathrm{ref}$')
 py.xlim(9,62)
 py.legend()
-py.loglog()
+#py.loglog()
 
 py.savefig('conver_heat.pdf')
 #py.show()
