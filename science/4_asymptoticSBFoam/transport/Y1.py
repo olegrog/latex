227b8be1b34388b5508603a280e8ef11b11f803c
Oleg Rogozin <o.a.rogozin@gmail.com>
2014-07-28 22:08:26 +0400

    [asymptoticSBFoam] revise figure titles

--- science/4_asymptoticSBFoam/transport/Y1.py
+++ science/4_asymptoticSBFoam/transport/Y1.py
@@ -16,11 +16,13 @@ data = py.loadtxt('Y1_2.txt')
 x,y = data.T
 p, = py.plot(x, y, '-', lw=2)
 
-py.xlabel(r'$\eta$', labelpad=-1)
 py.semilogy()
-py.axes().set_xticks([0,5,10,15])
-py.axes().set_yticks([1,1e-2,1e-4])
-py.ylabel(r'$\displaystyle\frac{Y_1}2$', y=.8, labelpad=-3, rotation=0)
-py.xlim(0,15)
+py.axes().set_xticks([0,5])
+py.axes().set_yticks([1,1e-1,1e-2])
+py.axes().xaxis.set_minor_locator(py.MultipleLocator(1))
+py.xlabel(r'$\eta$', labelpad=-5)
+py.ylabel(r'$\displaystyle\frac{Y_1}2$', y=.75, labelpad=-2, rotation=0)
+py.xlim(0,5)
+py.ylim(1e-2,1)
 
 py.savefig('Y1.pdf', bbox_inches='tight')
