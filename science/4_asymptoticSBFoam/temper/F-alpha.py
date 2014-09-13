2dea30f3da5164d163763f50ee8bbd4bd65d6c81
Oleg Rogozin <o.a.rogozin@gmail.com>
2014-09-13 12:57:42 +0400

    [asymptoticSBFoam] temper directory, new figures

---
+++ science/4_asymptoticSBFoam/temper/F-alpha.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import pylab as py
+params = {'backend': 'pdf',
+          'axes.labelsize': 10,
+          'text.fontsize': 10,
+          'legend.fontsize': 10,
+          'xtick.labelsize': 9,
+          'ytick.labelsize': 9,
+          'text.usetex': True,
+          'figure.figsize': [3,3]}
+py.rcParams.update(params)
+import numpy as np
+
+def func(n, x, A):
+    return A*x**n;
+
+def plot_dash_line(n, ind):
+    X = np.array([ x.min(), x.max() ])
+    Y = func(n, X, np.exp( np.log(F[ind]) - n*np.log(x[ind]) ))
+    py.plot(X, Y, 'k--', lw=.5)
+
+def plot(filename, label, factor):
+    data = py.loadtxt(filename)
+    alpha, F = data.T[0:4:3]
+    x, F = alpha-1, factor*F
+    py.plot(x, F, '-', lw=1.5, label=label)
+    return x, F
+
+x, F = plot('cylinders.txt', r'$\mathrm{cylinders}\;\alpha=5$', -2)
+plot_dash_line(1, 0)
+plot_dash_line(3, -1)
+
+#x, F = plot('spheres.txt', r'$\mathrm{spheres}\;\alpha=5$', -100)
+#plot_dash_line(1, 0)
+#plot_dash_line(3, -1)
+
+py.xlabel(r'$\alpha-1$', labelpad=5)
+py.ylabel(r'$-p_0F_{x2}$', y=.8, labelpad=0, rotation=0)
+py.loglog()
+py.xlim(1e-2, 1e2)
+py.ylim(1e-4, 1e8)
+ax = py.axes()
+#handles, labels = ax.get_legend_handles_labels()
+#ax.legend(handles, labels, loc=0)
+
+from matplotlib.ticker import LogLocator
+ax.yaxis.set_major_locator(LogLocator(1e4))
+
+py.savefig('F-alpha-noncoaxial.pdf', bbox_inches='tight')
+
---
+++ science/4_asymptoticSBFoam/temper/U-alpha.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import pylab as py
+params = {'backend': 'pdf',
+          'axes.labelsize': 10,
+          'text.fontsize': 10,
+          'legend.fontsize': 10,
+          'xtick.labelsize': 9,
+          'ytick.labelsize': 9,
+          'text.usetex': True,
+          'figure.figsize': [3,3]}
+py.rcParams.update(params)
+import numpy as np
+import scipy.optimize
+import functools
+import sys
+
+data = py.loadtxt(sys.argv[1] + '.txt')
+alpha, maxU = data.T[0:2]
+x = alpha - 1
+py.plot(x, maxU, '-', lw=1.5)
+
+def func(n, x, A):
+    return A*x**n;
+
+def plot_dash_line(n, ind):
+    X = np.array([ x.min(), x.max() ])
+    Y = func(n, X, np.exp( np.log(maxU[ind]) - n*np.log(x[ind]) ))
+    py.plot(X, Y, 'k--', lw=.5, label=r'$\phi = \pi/2$')
+
+plot_dash_line(3, 0)
+plot_dash_line(1.5, -1)
+
+py.xlabel(r'$\alpha-1$', labelpad=5)
+py.ylabel(r'$(|u_{i1}|)_{\mathrm{max}}$', y=.8, labelpad=0, rotation=0)
+py.loglog()
+py.xlim(1e-2, 1e2)
+py.ylim(1e-8, 1e4)
+ax = py.axes()
+
+from matplotlib.ticker import LogLocator
+ax.yaxis.set_major_locator(LogLocator(1e4))
+
+py.savefig('U-alpha-' + sys.argv[1] + '.pdf', bbox_inches='tight')
+
