5e583d4284b361aaa32ae5e78a176deab59bfd61
Oleg Rogozin <o.a.rogozin@gmail.com>
2015-03-30 13:04:40 +0300

    [couette] plot profiles

--- science/3_couette_heat/profiles/plot_profiles.py
+++ science/3_couette_heat/profiles/plot_profiles.py
@@ -1,104 +1,32 @@
 #!/usr/bin/env python
-# Usage: ./plot_profiles.py /Users/olegrog/kesolver/data/ .
 
-import sys, math, os, glob
+import os
+import sys
 import numpy as np
-import pylab as py
-from scipy.optimize import curve_fit
-sys.path.append('/Users/olegrog/kesolver/tools')
 
-import out2
-
-_, indir, outdir = sys.argv
-
-Kn = [ 0.1, 1, 10 ]
-U = [ 0.1, 1.0, 2.0, 5.0 ]
-L = 0.5
-s2 = np.sqrt(2)
-skip = 0.75         # skip this amount of timesteps from the beginning
-
-linestyle = {
-    0.1:    '-',
-    1.0:    '--',
-    2.0:    ':',
-    5.0:    '-.'
+params = {
+    'Pxy':      [ 'Pxy', 'center bottom', '-P_{xy}/\\Delta{v}', -0.2, 0.6, '-y', int(True) ],
+    'vx':       [ 'vx', 'center top', 'v_x/\\Delta{v}', 0, 0.5, 'y', int(True) ],
+    'qx':       [ 'qx', 'center top', '-q_x/(\\Delta{v})^2', 0, 0.35, '-y/v', int(True) ],
+    'qy':       [ 'qy', 'center top', '-q_y/(\\Delta{v})^2', 0, 0.15, '-y/v', int(True) ],
+    'Pxx':      [ 'Pxx', 'center bottom', '(P_{xx}-P_{yy})/(\\Delta{v})^2', -0.2, 0.5, 'y', int(False) ],
+    'Pzz':      [ 'Pzz', 'center bottom', '(P_{zz}-P_{yy})/(\\Delta{v})^2', -0.01, 0.04, 'y', int(False) ],
+    'tau':      [ 'tau', 'center bottom', '\\tau/(\\Delta{v})^2', 0, 0.18, 'y/v/v', int(False) ]
 }
+dummies = [ 'macro', 'place', 'ylabel', 'ymin', 'ymax', 'base', 'filter' ]
 
-linecolor = {
-    0.1:    'red',
-    1:      'green',
-    10:     'blue'
-}
+macro = sys.argv[1].split('.')[0]
+data = params[macro]
+    
+print 'Creating %s.sh...' % macro
+filedata = None
+with open('template.sh', 'r') as file:
+    filedata = file.read().replace('<name>', macro)
 
-show = True
+for i, dummy in enumerate(dummies):
+    filedata = filedata.replace('<%s>' % dummy, str(data[i]))
 
-result = {}
-macros = ['Pxy', 'vx', 'qx', 'qy', 'Pxx', 'Pyy', 'Pzz', 'T']
-for m in macros:
-    result[m] = {}
-
-def parabola_fit(x, a, b):
-    return a*x**2 + b
-
-def spline_fit(x, a, b, c):
-    return a*x**2 + b*x + c
-
-def average_data(path, nrows):
-    timesteps = sorted([int(f[1:].split('.')[0]) for f in os.listdir(path) if f.endswith('.txt')])
-    first, last = int(skip*len(timesteps)), len(timesteps)-1
-    read_macros = lambda ts: np.array(out2.readMacros(os.path.join(path, 'm%s.txt' % str(ts)), nrows))
-    data = read_macros(timesteps[-1])
-    for ts in timesteps[first:last]:
-        data += read_macros(ts)
-    print 'average with %d files' % (last - first + 1)
-    return data / (last - first + 1)
-
-for kn in Kn:
-    for u in U:
-        dirname = os.path.join(indir, 'couette' + "%.1f" % u, "%.3f" % kn)
-        nodes, cells = out2.readNodesCells(dirname + '/couette.kei')
-        Y = [0.5*kn*(nodes[cell.nodes[0]] + nodes[cell.nodes[2]])[1] for cell in cells ]
-        data = average_data(os.path.join(dirname, 'result'), len(cells))
-        data = (np.vstack([Y, data]).T)[np.array(Y).argsort()].T
-        XX, rho, vx, vy, vz, T, Txx, Tyy, Tzz, qx, qy, qz, Pyz, Pxz, Pxy, H = data
-        Pxx, Pyy, Pzz = rho*np.array([Txx, Tyy, Tzz])
-        ind = len(XX)/2
-        X = np.append(np.append(0, XX[ind:]), L)
-        def symm(A):
-            B = 0.5*((A+A[::-1])[ind:])
-            X = XX[ind:]
-            f0, fL = parabola_fit, spline_fit
-            fit0, cov = curve_fit(f0, X[:2], B[:2])
-            fitL, cov = curve_fit(fL, X[-3:], B[-3:])
-            return np.append(np.append(f0(0, *fit0), B), fL(L, *fitL))
-        def asym(A):
-            B = 0.5*((A-A[::-1])[ind:])
-            fL = spline_fit
-            fitL, cov = curve_fit(fL, XX[-3:], B[-3:])
-            return np.append(np.append(0, B), fL(L, *fitL))
-        
-        #py.plot(XX, T)
-        #vx, qx = asym(vx)/u/s2, asym(qx)/u/s2
-        #T, Pxy, Pxx, Pyy, Pzz, qy = symm(T), symm(Pxy), symm(Pxx), symm(Pyy), symm(Pzz), symm(qy)
-        result['vx'][u] = asym(vx)/u/s2
-        result['qx'][u] = asym(qx)/u/s2
-        result['qy'][u] = asym(qy)/u/s2
-        result['Pxy'][u] = symm(Pxy)/u
-        result['Pxx'][u] = symm(Pxx)/u
-        result['Pyy'][u] = symm(Pyy)/u
-        result['Pzz'][u] = symm(Pzz)/u
-        result['T'][u] = symm(T)/u
-        qy -= Pxy*vx
-        if show and u>=0.1:
-            Y = symm(Pxy)/u
-            py.plot(X, Y, label=r'$kn=%.1f, u=%.1f' % (kn,u), linestyle=linestyle[u], color=linecolor[kn], lw=1.2) #, marker='o')
-    for m in macros:
-        print m, np.transpose(result[m].values()).shape, np.array([X]).T.shape
-        #np.savetxt(outdir + '/profile_' + m + '-' + str(kn) + '.txt', np.hstack((np.transpose([X]), np.transpose(result[m].values()))),
-        np.savetxt(outdir + '/profile_' + m + '-' + str(kn) + '.txt', np.vstack([X, result[m][0.1], result[m][1.0], result[m][2.0], result[m][5.0]]).T,
-            fmt='%.5e', header='        x       U=0.1       U=1.0       U=2.0       U=5.0')
-
-if show:
-#    py.legend()
-    py.show()
-
+output = '%s.sh' % macro
+with open(output, 'w') as file:
+    file.write(filedata)
+os.chmod(output, 0755)
---
+++ science/3_couette_heat/profiles/template.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env gnuplot
+
+set term epslatex standalone size 4,3 font 10 color dashed
+set out "<name>.tex"
+set key <place> maxrows 4 width -1
+
+set xlabel '$y$' offset graph 0.0, 0.03
+set ylabel '$<ylabel>$' offset graph 0.05, 0.0
+
+#set xzeroaxis
+
+set xrange [0:0.5]
+set yrange [<ymin>:<ymax>]
+
+base(y,v) = <base>
+flag = <filter>
+filter(x) = x/flag
+
+set macros
+dummy = "NaN title (flag ? ' ' : '') lt -3"
+
+plot @dummy, \
+    NaN title "$\\mathrm{Kn}=0.1$"   w l lw 2 lt 1 lc 1, \
+    NaN title "$\\mathrm{Kn}=1$"     w l lw 2 lt 1 lc 2, \
+    NaN title "$\\mathrm{Kn}=10$"    w l lw 2 lt 1 lc 3, \
+    NaN title ( flag ? "$\\Delta v=0.1$" : "" ) w l lw 2 lt 2 lc 0, \
+    NaN title "$\\Delta v=1$"       w l lw 2 lt 3 lc 0, \
+    NaN title "$\\Delta v=2$"       w l lw 2 lt 4 lc 0, \
+    NaN title "$\\Delta v=5$"       w l lw 2 lt 5 lc 0, \
+    "profile_<macro>-0.1.txt" using (filter($1)):(base($2,0.1)) notitle w l lw 2 lt 2 lc 1, \
+    "profile_<macro>-0.1.txt" using 1:(base($3,1.0)) notitle w l lw 2 lt 3 lc 1, \
+    "profile_<macro>-0.1.txt" using 1:(base($4,2.0)) notitle w l lw 2 lt 4 lc 1, \
+    "profile_<macro>-0.1.txt" using 1:(base($5,5.0)) notitle w l lw 2 lt 5 lc 1, \
+    "profile_<macro>-1.txt"   using (filter($1)):(base($2,0.1)) notitle w l lw 2 lt 2 lc 2, \
+    "profile_<macro>-1.txt"   using 1:(base($3,1.0)) notitle w l lw 2 lt 3 lc 2, \
+    "profile_<macro>-1.txt"   using 1:(base($4,2.0)) notitle w l lw 2 lt 4 lc 2, \
+    "profile_<macro>-1.txt"   using 1:(base($5,5.0)) notitle w l lw 2 lt 5 lc 2, \
+    "profile_<macro>-10.txt"  using (filter($1)):(base($2,0.1)) notitle w l lw 2 lt 2 lc 3, \
+    "profile_<macro>-10.txt"  using 1:(base($3,1.0)) notitle w l lw 2 lt 3 lc 3, \
+    "profile_<macro>-10.txt"  using 1:(base($4,2.0)) notitle w l lw 2 lt 4 lc 3, \
+    "profile_<macro>-10.txt"  using 1:(base($5,5.0)) notitle w l lw 2 lt 5 lc 3
+
