5704fd5e112a1afb00d551220dffd829526e7ebf
Oleg Rogozin <o.a.rogozin@gmail.com>
2015-06-18 03:37:24 +0300

    [sone_bobylev] replace pyevtk with the native VTK

--- science/7_sone_bobylev/contours/knudsen_layer_correction.py
+++ science/7_sone_bobylev/contours/knudsen_layer_correction.py
@@ -3,20 +3,29 @@
 import numpy as np
 import sys, os, vtk
 from scipy.interpolate import griddata, interp1d
-from vtk.util.numpy_support import vtk_to_numpy
-from pyevtk.hl import pointsToVTK
+from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
 
 _, infile, outfile, kn = sys.argv
 k = np.sqrt(np.pi)*float(kn)/2
 
-def get_point_data(reader, name):
-    vtk_array = reader.GetOutput().GetPointData().GetArray(name)
+def get_point_data(out, name):
+    vtk_array = out.GetPointData().GetArray(name)
     return vtk_to_numpy(vtk_array)
 
-def get_points(reader):
-    vtk_array = reader.GetOutput().GetPoints().GetData()
+def get_points(out):
+    vtk_array = out.GetPoints().GetData()
     numpy_array = vtk_to_numpy(vtk_array)
     return numpy_array[:,0], numpy_array[:,1], numpy_array[:,2]
+
+def get_cell_data(out, name):
+    vtk_array = out.GetCellData().GetArray(name)
+    return vtk_to_numpy(vtk_array)
+
+def get_cell_centers(out):
+    centers = vtk.vtkCellCenters()
+    centers.AddInputData(out)
+    centers.Update()
+    return get_points(centers.GetOutput())
 
 def interp(eta, F):
     log_interp = interp1d(eta, np.log(F), kind='linear')
@@ -25,44 +34,78 @@ def interp(eta, F):
     return lambda x: np.where(x < eta_max, np.exp(log_interp(f(x))), 0)
 
 def vectorize(X, Y):
-    values = dict(zip(X, Y))
-    return np.vectorize(lambda x: values[x])
+    return interp1d(X, Y, kind='cubic')
 
-vec2tuple = lambda v: (v[:,0], v[:,1], v[:,2])
+def add_array(data, F, name):
+    vtk_array = numpy_to_vtk(F, array_type=vtk.VTK_FLOAT)
+    vtk_array.SetName(name)
+    data.AddArray(vtk_array)
 
+def add_correction(x, y, T0, p0, U1):
+    eta = p0/wallT0(x)*y/k
+    T = T0 - k*wallT0(x)/p0*wallDTn(x)*Theta_1(eta)
+    rho = p0/T0 + k/wallT0(x)*wallDTn(x)*Omega_1(eta)
+    U = k*U1/p0
+    U[:,0] += -k*np.sqrt(wallT0(x))/p0*wallDTt(x)*Y_1(eta)
+    return rho, T, U
+
+### Read the Knudsen-layer functions
 eta, Y_0, Y_1, Omega_1, Theta_1, H_A, H_B, Y_a4 = np.loadtxt('../tables/kn-layer-hs.txt').T
 Y_1 = interp(eta, Y_1)
 Theta_1 = interp(eta, Theta_1)
 Omega_1 = interp(eta, Omega_1)
 
+### Read a VTK-file
 reader = vtk.vtkUnstructuredGridReader()
 reader.SetFileName(infile)
 reader.Update()
+out = reader.GetOutput()
 
-x, y, z = get_points(reader)
-T0 = get_point_data(reader, 'T0')
-U1 = get_point_data(reader, 'U1')
-p0 = get_point_data(reader, 'p0')[0]
-gradT0 = get_point_data(reader, 'grad(T0)')
+### Add points and cells to the new grid
+grid = vtk.vtkUnstructuredGrid()
+grid.SetPoints(out.GetPoints())
+grid.SetCells(out.GetCellType(0), out.GetCells())
 
+### Read point data
+x, y, z = get_points(out)
+T0 = get_point_data(out, 'T0')
+U1 = get_point_data(out, 'U1')
+p0 = get_point_data(out, 'p0')[0]
+gradT0 = get_point_data(out, 'grad(T0)')
+
+### Find wall functions
 wall = np.where((y==0) & (z==0))
 X = x[wall]
 wallT0 = vectorize(X, T0[wall])
 wallDTt = vectorize(X, gradT0[wall][:,0])
 wallDTn = vectorize(X, gradT0[wall][:,1])
-wallU1 = vectorize(X, U1[wall][:,0])
 
-X = np.sort(X)
+### Debug info
+#X = np.sort(X)
 #print X, wallT0(X), wallDTt(X), wallDTn(X)
 
-eta = p0/wallT0(x)*y/k
-T = T0 - k*wallT0(x)/p0*wallDTn(x)*Theta_1(eta)
-rho = p0/T0 + k/wallT0(x)*wallDTn(x)*Omega_1(eta)
-U = k*U1/p0
-U[:,0] += -k*np.sqrt(wallT0(x))/p0*wallDTt(x)*Y_1(eta)
+### Add point data
+rho, T, U = add_correction(x, y, T0, p0, U1)
+data = grid.GetPointData()
+add_array(data, T, 'T')
+add_array(data, U, 'U')
+add_array(data, rho, 'rho')
 
+### Read point data
+x, y, z = get_cell_centers(out)
+T0 = get_cell_data(out, 'T0')
+U1 = get_cell_data(out, 'U1')
 
-pointsToVTK(os.path.splitext(outfile)[0], x, y, z, data = {
-    'T': T, 'U': vec2tuple(U), 'rho': rho
-})
+### Add cell data
+rho, T, U = add_correction(x, y, T0, p0, U1)
+data = grid.GetCellData()
+add_array(data, T, 'T')
+add_array(data, U, 'U')
+add_array(data, rho, 'rho')
+
+### Write a VTK-file
+writer = vtk.vtkUnstructuredGridWriter()
+writer.SetInputData(grid)
+writer.SetFileName(outfile)
+writer.Update()
 
