5b67461d5f3063d6b0585e819b69255bfaa2222f
Oleg Rogozin <o.a.rogozin@gmail.com>
2015-11-06 17:37:15 +0300

    [sone_bobylev] rename SNIT --> SNIF

--- science/7_sone_bobylev/vs_kn/template.sh
+++ science/7_sone_bobylev/vs_kn/template.sh
@@ -28,8 +28,8 @@ dummy = "NaN title ' ' lt -3"
 
 plot \
     "heat.txt" using 1:(base($<column>)) title ( <is_heat> ? "Heat-conduction equation" : "" ) w l lw 3 lt 4 lc 5, \
-    "asym.txt" using 1:(base($<column>)) title "SNIT equations with $T_{B1}\\ne0$" w l lw 2 lt 1 lc 3, \
-    "nonslip.txt" using 1:(base($<column>)) title "SNIT equations with $T_{B1}=0$" w l lw 3 lt 2 lc 2, \
+    "asym.txt" using 1:(base($<column>)) title "SNIF equations with $T_{B1}\\ne0$" w l lw 2 lt 1 lc 3, \
+    "nonslip.txt" using 1:(base($<column>)) title "SNIF equations with $T_{B1}=0$" w l lw 3 lt 2 lc 2, \
     "data.txt" using 1:(base($<column>)) title "Uniform velocity grid" w lp lw 3 lt 2 lc 1 pt 6, \
     "high.txt" using 1:(base($<column>)) title "Nonuniform velocity grid" w lp lw 3 lt 3 lc 4 pt 4
 
