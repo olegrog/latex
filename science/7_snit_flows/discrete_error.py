c37dd802b9da92ed7c879f612bfc81d76af1af10
Oleg Rogozin <o.a.rogozin@gmail.com>
2016-05-19 05:25:29 +0300

    [snit_flows] use np.einsum for discrete_error.py

--- science/7_snit_flows/discrete_error.py
+++ science/7_snit_flows/discrete_error.py
@@ -3,139 +3,138 @@ r"Script for evaluating the errors in the various moments calculation of the dis
 r"Usage: ./discrete_error.py <U> <N_r> <N_y> <cut_r> <ratio>"
 r"Example:  ./discrete_error.py .5 12 22 4.3 1.3"
 
+import sys
 import numpy as np
 from functools import partial
-import sys
+from collections import namedtuple
 
-Rho, Temp, Speed = 1., 1., np.array([0,0,0],dtype=float)
-Qflow, Tau = np.array([0.1, 0.2, 0]), np.array([0.05, 0., 0.2])
-_, temp_ratio, N_r, N_y, cut_r, q = sys.argv
+_, temp_ratio, N_r, N_y, cut_r, q_r, q_y = sys.argv
+
+Rho, Temp, Speed = 1., 1., np.zeros(3)
+Speed[0] = 1e-1
+Qflow, Tau = np.array([0.1, 0.2, 0]), np.array([0.05, 0., 0.2])*0
 temp_ratio = float(temp_ratio)
-N_r = int(N_r)
-N_y = int(N_y)
-cut_r = float(cut_r)
-q = float(q)
-Speed[0] = 1e-2
 heat = temp_ratio*Speed[0]
-cut_x = cut_r
-N_x = N_z = N_r
-gr = q # type of the grid
+N_r, N_y = int(N_r), int(N_y)
+cut_r, cut_y = float(cut_r), float(cut_r)
+q_r, q_y = float(q_r), float(q_y)
 
-Tauij = [[0,Tau[2],Tau[1]],[Tau[2],0,Tau[0]],[Tau[1],Tau[0],0]]
-zeros, ones = np.full(3, 0), np.full(3, 1)
-dimension = (2*N_x, 2*N_y, 2*N_z)
+zeros, ones = np.zeros_like(Speed), np.ones_like(Speed)
+hodge = np.zeros((3,3,3))
+hodge[0, 1, 2] = hodge[1, 2, 0] = hodge[2, 0, 1] = 1
+
+### Define different grids
+
+Grid = namedtuple('Grid', ['i2h','i2xi'])
+
+grids = {
+    'uniform': Grid(
+        i2h =  lambda q, cut, N: cut/N + idx(N)*0,
+        i2xi = lambda q, cut, N: idx(N)*cut/N
+    ),
+    'geometric': Grid(
+        i2h =  lambda q, cut, N: h1(q, cut, N) * symm_h(q**np.arange(N)),
+        i2xi = lambda q, cut, N: h1(q, cut, N) * (1 if q==1 else symm_xi((q**np.arange(N+1)-1)/(q-1)))
+    ),
+    'hermite': Grid(
+        i2h =  lambda q, cut, N: hermite(cut, N)[0],
+        i2xi = lambda q, cut, N: hermite(cut, N)[1]
+    ),
+    'quadratic': Grid(
+        i2h =  lambda q, cut, N: quadratic(q, cut, N)[0],
+        i2xi = lambda q, cut, N: quadratic(q, cut, N)[1]
+    )
+}
 
 def hermite(cut, N):
     H_xi, H_h = np.polynomial.hermite.hermgauss(2*N)
-    H_xi *= np.sqrt(Temp)
     H_h /= np.exp(-H_xi**2)
-    if np.sum(H_h) < 2*cut:
-        C = (2*cut) / np.sum(H_h)
-        H_xi *= C
-        H_h *= C
-    return np.vectorize(lambda i: H_xi[i]), np.vectorize(lambda i: H_h[i])
+    C = 2*cut / np.sum(H_h)
+    H_xi *= C
+    H_h *= C
+    return H_h, H_xi
 
-# width of the first cell
-h1 = lambda q: cut_r/N_y if q==1 else cut_r*(q-1)/(q**N_y-1)
+def quadratic(q, cut, N):
+    p=2
+    if cut-N*q < 0:
+       raise NameError('q = %g is too big' % q) 
+    A = (cut-N*q) / np.sum(np.arange(N)**p)
+    h = q + A*np.arange(N)**p
+    X = np.append(0, np.cumsum(h))
+    return symm_h(h), symm_xi(X)
 
-def i2h(i, q):
-    j = lambda i: abs(i-N_y+.5)+.5
-    nonuniform = lambda i: h1(q)*q**(j(i)-1)
-    uniform = lambda i: cut_x/N_x + i*0
-    return {
-        0: hermite(cut_r, N_z)[1],  # hermite grid
-        -1: uniform                 # uniform grid
-    }.get(q, nonuniform)(i)         # exp refinement
+symm_h = lambda x: np.hstack((x[::-1], x))
+semi_sum = lambda x: .5*(x[:-1] + x[1:])
+symm_xi = lambda x: np.hstack((-semi_sum(x)[::-1], semi_sum(x)))
+h1 = lambda q, cut, N: cut/N if q==1 else cut*(q-1)/(q**N-1)
+idx = lambda N: np.arange(2*N) - N + .5
 
-def i2xi(i, q):
-    sgn = lambda i: np.sign(i-N_y+.5)
-    j = lambda i: abs(i-N_y+.5)+.5
-    j_x = lambda i: i-N_x+.5
-    nonuniform = lambda i: sgn(i)*h1(q) * (j(i)-.5 if q==1 else (q**j(i)+q**(j(i)-1)-2)/(q-1)/2)
-    uniform = lambda i: j_x(i)*cut_x/N_x
-    return {
-        0: hermite(cut_r, N_z)[0],  # hermite grid
-        -1: uniform                 # uniform grid
-    }.get(q, nonuniform)(i)         # exp refinement
+### Create grid
 
-dxi = lambda i,j,k: i2h(i,gr)*i2h(j,q)*i2h(k,gr)
-xi = lambda i,j,k,v=zeros: (i2xi(i,gr)-v[0], i2xi(j,q)-v[1], i2xi(k,gr)-v[2])
-xi_x = lambda i,j,k,v=zeros: i2xi(i,gr)-v[0]
-xi_y = lambda i,j,k,v=zeros: i2xi(j,q)-v[1]
-sqr_xi = lambda i,j,k,v=zeros,s=ones: (s[0]*(i2xi(i,gr)-v[0]))**2 + (s[1]*(i2xi(j,q)-v[1]))**2 + (s[2]*(i2xi(k,gr)-v[2]))**2
-Maxwell = lambda v,t,i,j,k: Rho/(np.pi*t)**1.5 * np.exp(-sqr_xi(i,j,k,v)/t)*dxi(i,j,k)
+gtype_r = gtype_y = 'quadratic'
+H_r, Xi_r = grids[gtype_r].i2h(q_r, cut_r, N_r), grids[gtype_r].i2xi(q_r, cut_r, N_r)
+H_y, Xi_y = grids[gtype_y].i2h(q_y, cut_y, N_y), grids[gtype_y].i2xi(q_y, cut_y, N_y)
+E_r, E_y = np.ones_like(Xi_r), np.ones_like(Xi_y)
 
-radii = np.array([cut_x, cut_r, cut_r])
+dxi = np.einsum('i,j,k', H_r, H_y, H_r)
+xi = lambda v=zeros: np.einsum('li,lj,lk->ijkl', (Xi_r-v[0], E_r, E_r), (E_y, Xi_y-v[1], E_y), (E_r, E_r, Xi_r-v[2]))
+sqr_xi = lambda v=zeros,s=ones: np.linalg.norm(np.einsum('ijkl,l->ijkl', xi(v), s), axis=3)**2
+Maxwell = lambda vel, temp, rho=Rho: rho/(np.pi*temp)**1.5 * np.exp(-sqr_xi(vel)/temp)*dxi
 
-ind = np.fromfunction(lambda i,j,k: sqr_xi(i,j,k,zeros,radii**-1) <= 1, dimension)
-u = np.fromfunction(lambda i,j,k: xi(i,j,k), dimension)
-Dxi = np.fromfunction(lambda i,j,k: dxi(i,j,k), dimension)
-
-X_x = np.fromfunction(lambda i: i2xi(i,gr), (2*N_x,))
-X_y = np.fromfunction(lambda i: i2xi(i,q), (2*N_y,))
-X_z = np.fromfunction(lambda i: i2xi(i,gr), (2*N_z,))
-H_x = np.fromfunction(lambda i: i2h(i,gr), (2*N_x,))
-H_y = np.fromfunction(lambda i: i2h(i,q), (2*N_y,))
-H_z = np.fromfunction(lambda i: i2h(i,gr), (2*N_z,))
+radii = np.array([cut_r, cut_y, cut_r])
+ball = sqr_xi(zeros, 1./radii) <= 1
 
 print_grid = False
 if print_grid:
-    print "--- x:", X_x, H_x
-    print "--- y:", X_y, H_y
-    print "--- z:", X_z, H_z
-#print 0.5*(H_y[1:]+H_y[:-1])   # should coincide
-def calc_sizes(N,q):
-    h_min, h_max = i2h(N,q), i2h(0,q)
-    return (h_min, h_max, h_max/h_min)
-print "Cut_x = %g, cut_r = %g, ratio = %g, N_z = %d" % (cut_x, cut_r, q, N_z)
-print "Cell size (x): min = %.4g, max = %.4g, ratio = %.3g" % calc_sizes(N_x, gr)
-print "Cell size (y): min = %.4g, max = %.4g, ratio = %.3g" % calc_sizes(N_y, q)
-print "Cell size (z): min = %.4g, max = %.4g, ratio = %.3g" % calc_sizes(N_z, gr)
+    print "--- r:", H_r, Xi_r
+    print "--- y:", H_y, Xi_y
+
+sizes = lambda H: (H.min(), H.max(), H.max()/H.min())
+print "cut_r = %g, cut_y = %g, q_r = %g, q_y = %g" % (cut_r, cut_y, q_r, q_y)
+print "Cell size (r): min = %.4g, max = %.4g, ratio = %.3g" % sizes(H_r)
+print "Cell size (y): min = %.4g, max = %.4g, ratio = %.3g" % sizes(H_y)
 total = lambda X, cut: np.sum(np.abs(X) <= cut)/2
-print "Total cells: %d (%d, %d, %d):" % (np.sum(ind), total(X_x, cut_x), total(X_y, cut_r), total(X_z, cut_r))
+print "Total cells: %d (%d, %d, %d):" % (np.sum(ball), N_r, N_y, N_r)
 
 def splot(f):
     import pylab as py
     import mpl_toolkits.mplot3d.axes3d as p3
     fig = py.figure()
     ax = p3.Axes3D(fig)
-    Xi = np.fromfunction(lambda i: i2xi(i,gr), (2*N_x,))
-    Yi = np.fromfunction(lambda i: i2xi(i,q), (2*N_y,))
-    xv, yv = np.meshgrid(Xi, Yi, sparse=False, indexing='ij')
-    f[np.invert(ind[:,:,N_z])] = np.NaN
-    f[ind[:,:,N_z]] /= Dxi[ind[:,:,N_z]]
+    xv, yv = np.meshgrid(Xi_r, Xi_y, sparse=False, indexing='ij')
+    f /= dxi
+    f[np.invert(ball[:,:,N_z])] = np.NaN
     #ax.plot_wireframe(xv, yv, np.log(f[:,:,N_z]), linewidth=0.25)
     ax.plot_wireframe(xv, yv, f[:,:,N_z], linewidth=0.25)
     py.savefig('tmp.pdf')
     py.show()
 
-def err(theor, real):
-    return np.vectorize(lambda x,y: np.NaN if x==0 else abs((x-y)/x))(theor, real)
-    #return np.vectorize(lambda x,y: y if x==0 else abs((x-y)/x))(theor, real)
+err = lambda theor, real: (real-theor)/theor
 
 def calc_macro(f):
-    rho = np.sum(f[ind])
-    speed, qflow, tau = [0,0,0], [0,0,0], [0,0,0]
-    for i in xrange(3):
-        speed[i] = np.sum((f*u[i])[ind])/rho
-    c = np.fromfunction(lambda i,j,k: xi(i,j,k,speed), dimension)
-    sqr_c = np.fromfunction(lambda i,j,k: sqr_xi(i,j,k,speed), dimension)
-    temp = 2./3*np.sum((f*sqr_c)[ind])/rho
-    for i in xrange(3):
-        qflow[i] = np.sum((f*c[i]*sqr_c)[ind])
-        tau[i] = np.sum(2*(f*c[(i+1)%3]*c[(i+2)%3])[ind])
+    f = np.einsum('ijk,ijk->ijk', f, ball)
+    rho = np.einsum('ijk->', f)
+    speed = np.einsum('ijk,ijkl', f, xi())/rho
+    c, sqr_c = xi(speed), sqr_xi(speed)
+    csqr_c = np.einsum('ijk,ijkl->ijkl', sqr_c, c)
+    cc_ij = np.einsum('ijkl,ijkm->ijklm', c, c)
+    cc = np.einsum('ijklm,lmn', cc_ij, hodge)
+    temp = 2./3*np.einsum('ijk,ijk', f, sqr_c)/rho
+    qflow = np.einsum('ijk,ijkl', f, csqr_c)
+    tau = 2*np.einsum('ijk,ijkl', f, cc)
     return rho, temp, speed, qflow, tau
 
 def test_1(Temp, Speed):
     P = Rho * Temp
-    f1 = 1./P/Temp * np.fromfunction(lambda i,j,k: np.sum(np.tensordot(Tauij, xi(i,j,k,Speed), (0,0)) * xi(i,j,k,Speed), 0), dimension)
-    f2 = 4./5/P/Temp * np.fromfunction(lambda i,j,k: np.tensordot(Qflow, xi(i,j,k,Speed), 1)*(sqr_xi(i,j,k,Speed)/Temp-2.5), dimension)
-    f = np.fromfunction(partial(Maxwell, Speed, Temp), dimension) * (1 + f1 + f2)
+    Xi, Sqr_xi = xi(Speed), sqr_xi(Speed)
+    f1 = 1./P/Temp * np.einsum('ijkl,ijkm,n,lmn->ijk', Xi, Xi, Tau, hodge)
+    f2 = 4./5/P/Temp * (np.einsum('ijkl,ijk,l->ijk', Xi, Sqr_xi/Temp-2.5, Qflow))
+    f = Maxwell(Speed, Temp) * (1 + f1 + f2)
     rho, temp, speed, qflow, tau = calc_macro(f)
     #splot(f)
     print "\n-- Test #1: continual flows - Grad's 13-moment approximation (temp = %g, speed = %g)" % (Temp, Speed[0])
-    print "rho =", err(Rho, rho)
-    print "temp =", err(Temp, temp)
+    print "rho =", err(Rho, rho), Rho, rho
+    print "temp =", err(Temp, temp), Temp, temp
     print "speed =", err(Speed, speed)
     print "qflow =", err(Qflow, qflow/rho)
     print "tau =", err(Tau, tau/rho)
@@ -147,10 +146,9 @@ def test_2(Temp):
     delta_temp = Temp2 - Temp1
 
     Rho1, Rho2 = 2*Rho*np.sqrt(Temp2)/ss_temp, 2*Rho*np.sqrt(Temp1)/ss_temp
-    f = Rho1/(np.pi*Temp1)**1.5 * np.fromfunction(lambda i,j,k: np.exp(-sqr_xi(i,j,k,Speed)/(Temp1))*dxi(i,j,k), dimension)
-    negative = np.fromfunction(lambda i,j,k: j<N_y, dimension)
-    f[negative] = Rho2/(np.pi*Temp2)**1.5 * np.fromfunction(lambda i,j,k: np.exp(-sqr_xi(i,j,k,-Speed)/(Temp2))*dxi(i,j,k), dimension)[negative]
-
+    f = Maxwell(Speed, Temp1, Rho1)
+    negative = xi()[...,1] < 0
+    f[negative] = Maxwell(-Speed, Temp2, Rho2)[negative]
     rho, temp, speed, qflow, tau = calc_macro(f)
 
     Rho_ = Rho
@@ -177,8 +175,9 @@ def test_3():
     gamma_1 = 1.270042427
     eta_, A_, B_, D1_, D2_, F_ = np.loadtxt("../tables/ci-functions.txt").T
     B = np.poly1d(np.polyfit(eta_, B_, 16))
-    phi = np.fromfunction(lambda i,j,k: 2*Speed[0]*xi_x(i,j,k)*(1 - xi_y(i,j,k)*B(np.sqrt(sqr_xi(i,j,k)))*kn), dimension)
-    f = np.fromfunction(partial(Maxwell, zeros, Temp), dimension) * (1 + phi)
+    Xi, Sqr_xi = xi(), sqr_xi()
+    phi = 2*Speed[0]* Xi[...,0] * (1-Xi[...,1]*B(np.sqrt(Sqr_xi)))
+    f = Maxwell(zeros, Temp) * (1 + phi)
     rho, temp, speed, qflow, tau = calc_macro(f)
 
     print "\n-- Test #3: linear case - asymptotic solution for hard-sphere molecules"
@@ -186,11 +185,12 @@ def test_3():
     print "speed =", err(Speed, speed)
     print "tau =", err((0, 0, -2*gamma_1*Speed[0]*kn), tau/rho)
 
-#test_1(Temp/temp_ratio, Speed)
-test_1(Temp, Speed)
-test_1(Temp*temp_ratio, Speed)
-test_1(Temp*np.sqrt(temp_ratio), Speed)
-Speed[1] = 1e-3
-test_2(Temp)
-test_2(Temp*temp_ratio)
-test_2(Temp*np.sqrt(temp_ratio))
+with np.errstate(divide='ignore', invalid='ignore'):
+    test_1(Temp*(temp_ratio-1), Speed)
+    test_1(Temp, Speed)
+    test_1(Temp*temp_ratio, Speed)
+    #test_1(Temp*np.sqrt(temp_ratio), Speed)
+    Speed[1] = 1e-2
+    test_2(Temp)
+    #test_2(Temp*temp_ratio)
+    #test_2(Temp*np.sqrt(temp_ratio))
