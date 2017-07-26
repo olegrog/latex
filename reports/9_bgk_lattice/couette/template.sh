f3497c8b53b00d33465a075f9d13cd37ca9efe0f
Oleg Rogozin <o.a.rogozin@gmail.com>
2017-07-27 00:36:42 +0300

    [bgk_lattice] update txt-data for d3q19+dvm

--- reports/9_bgk_lattice/couette/template.sh
+++ reports/9_bgk_lattice/couette/template.sh
@@ -15,17 +15,18 @@ set lmargin 6
 set bmargin 2
 
 set xrange [0:.5]
-set yrange [0:.6]
+set yrange [0:.5]
 
 U = 0.02
 our_width = 1
 our_point = 6
 exact_point = 5
 exact_color = 0
+k = 40
 
 plot \
     "<name>.txt" using 1:($2/U) title '$v_x/\Delta v$' w lp lw our_width pt our_point, \
     "<name>.txt" using 1:(-$3/U) title '$-p_{xy}/\Delta v$' w lp lw our_width pt our_point, \
-    "<name>.txt" using 1:(-$4/U/U) title '$-q_x/(\Delta v)^2$' w lp lw our_width pt our_point, \
+    "<name>.txt" using 1:(-k*$4/U) title sprintf('$-%dq_x/\Delta v$', k) w lp lw our_width pt our_point, \
     "k1e-1.txt" using 1:2 notitle w p pt exact_point lc exact_color, \
     "k1e-1.txt" using 1:(-2*$3) notitle w p pt exact_point lc exact_color
