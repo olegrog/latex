cacc7e4bf867ace9ec4912d70e426b253e0f75d8
Oleg Rogozin <o.a.rogozin@gmail.com>
2018-09-25 16:31:43 +0300

    [slm] depth of tracks obtained by simulation

--- science/9_slm/experiment/template.sh
+++ science/9_slm/experiment/template.sh
@@ -20,12 +20,12 @@ lwe = 3
 lwn = 3
 powers = system("awk '!/^#/ { print $1 }' ".exper." | sort -g | uniq")
 base(power, speed) = <base>
-func(width) = width - 55
+func(value) = <func>
 
 plot for [p in powers] \
-    sprintf("<(grep '^%s' '%s')", p, exper) u (base(p,$2)):(func($3)) w lp lw lwe pt 6 title p.' W (experiment)', \
-    numer.'60W'.crude.'.txt'  u (base(60,$1)) :(func($2)) w lp lw lwn pt 5 dt 2 lt 1 title '60 W (simulation)', \
-    numer.'90W'.crude.'.txt'  u (base(90,$1)) :(func($2)) w lp lw lwn pt 5 dt 2 lt 2 title '90 W (simulation)', \
-    numer.'90W'.fine.'.txt'   u (base(90,$1)) :(func($2)) w lp lw lwn pt 3 dt 3 title '90 W (fine mesh)', \
-    numer.'150W'.crude.'.txt' u (base(120,$1)):(func($2)) w lp lw lwn pt 5 dt 2 lt 4 title '150 W (simulation)'
+    sprintf("<(grep '^%s' '%s')", p, exper) u (base(p,$2)):(func($<c_exp>)) w lp lw lwe pt 6 title p.' W (experiment)', \
+    numer.'60W'.crude.'.txt'  u (base(60,$1)) :(func($<c_num>)) w lp lw lwn pt 5 dt 2 lt 1 title '60 W (simulation)', \
+    numer.'90W'.crude.'.txt'  u (base(90,$1)) :(func($<c_num>)) w lp lw lwn pt 5 dt 2 lt 2 title '90 W (simulation)', \
+    numer.'90W'.fine.'.txt'   u (base(90,$1)) :(func($<c_num>)) w lp lw lwn pt 3 dt 3 title '90 W (fine mesh)', \
+    numer.'150W'.crude.'.txt' u (base(120,$1)):(func($<c_num>)) w lp lw lwn pt 5 dt 2 lt 4 title '150 W (simulation)'
 
