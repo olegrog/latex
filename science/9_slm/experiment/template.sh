0670b9dfdd5e2601d552e7d771ecc0e1f8b8169e
Oleg Rogozin <o.a.rogozin@gmail.com>
2018-10-16 16:32:34 +0300

    [slm] update simulation data for A(psi)

--- science/9_slm/experiment/template.sh
+++ science/9_slm/experiment/template.sh
@@ -26,6 +26,7 @@ plot for [p in powers] \
     sprintf("<(grep '^%s' '%s')", p, exper) u (base(p,$2)):(func($<c_exp>)) w lp lw lwe pt 6 title p.' W (experiment)', \
     numer.'60W'.crude.'.txt'  u (base(60,$1)) :(func($<c_num>)) w lp lw lwn pt 5 dt 2 lt 1 title '60 W (simulation)', \
     numer.'90W'.crude.'.txt'  u (base(90,$1)) :(func($<c_num>)) w lp lw lwn pt 5 dt 2 lt 2 title '90 W (simulation)', \
-    numer.'90W'.fine.'.txt'   u (base(90,$1)) :(func($<c_num>)) w lp lw lwn pt 3 dt 3 title '90 W (fine mesh)', \
-    numer.'150W'.crude.'.txt' u (base(120,$1)):(func($<c_num>)) w lp lw lwn pt 5 dt 2 lt 4 title '150 W (simulation)'
+    numer.'120W'.crude.'.txt' u (base(120,$1)):(func($<c_num>)) w lp lw lwn pt 5 dt 2 lt 3 title '120 W (simulation)', \
+    numer.'150W'.crude.'.txt' u (base(150,$1)):(func($<c_num>)) w lp lw lwn pt 5 dt 2 lt 4 title '150 W (simulation)'
 
+#    numer.'90W'.fine.'.txt'   u (base(90,$1)) :(func($<c_num>)) w lp lw lwn pt 3 dt 3 title '90 W (fine mesh)', \
