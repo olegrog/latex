e4323441922497836bff9c0d93dc352d4077ada0
Oleg Rogozin <o.a.rogozin@gmail.com>
2018-11-27 18:36:24 +0300

    [dvm-lbm] After the second revision

--- reports/9_bgk_lattice/couette/template-shear.sh
+++ reports/9_bgk_lattice/couette/template-shear.sh
@@ -2,11 +2,10 @@
 
 set term epslatex standalone size 4.5, 3 font 9 color dashed
 set out "<name>.tex"
-set key center bottom
+set key center top
 
 set xlabel '$y$' offset graph 0.48, 0.15
-set ylabel '$\displaystyle\frac{p_{xy}-p^{\mathrm{(ex)}}_{xy}}{p^{\mathrm{(ex)}}_{xy}}$' \
-    offset graph 0.68, 0.44 rotate by 0
+set ylabel '$p_{xy} - p^{\mathrm{(ex)}}_{xy}$' offset graph 0.54, 0.46 rotate by 0
 set colors classic
 
 set border 3
@@ -19,7 +18,7 @@ set lmargin 7
 set bmargin 2
 
 set xrange [0:.5]
-#set yrange [0:.5]
+set yrange [-0.0004:0.0004]
 
 our_width = 2
 our_point = 6
@@ -28,6 +27,7 @@ U = 0.02
 factor = sqrt(2)
 exact = <exact>
 func(x) = (x/U - 2*exact)/(2*exact)
+func(x) = x/U - 2*exact
 
 x_0 = <buffer>
 if (<hybrid>) set arrow from x_0, graph 0 to x_0, graph 1 nohead dt 4 lc 0 lw 3
