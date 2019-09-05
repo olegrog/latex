ec59aa2c2b8f9e9e619341b311a9687c4e2c3ee0
Oleg Rogozin <o.a.rogozin@gmail.com>
2019-09-05 10:36:56 +0300

    [dvm-lbm] update Results and discussion section

--- reports/9_bgk_lattice/couette/template-norms.sh
+++ reports/9_bgk_lattice/couette/template-norms.sh
@@ -30,9 +30,9 @@ x_0 = <buffer>
 if (<hybrid>) set arrow from x_0, graph 0 to x_0, graph 1 nohead dt 4 lc 0 lw 3
 
 plot \
-    "<name>.txt" using 1:2 title '$E_\infty$' w lp lw our_width pt our_point, \
+    "<name>.txt" using 1:2 title '$E_1$' w lp lw our_width pt our_point, \
     "<name>.txt" using 1:3 title '$E_2$' w lp lw our_width pt our_point, \
-    "<name>.txt" using 1:4 title '$E_1$' w lp lw our_width pt our_point, \
+    "<name>.txt" using 1:4 title '$E_\infty$' w lp lw our_width pt our_point, \
     "<name>.txt"[7:] using 1:(abs($4)) title '$|q_x|$' w lp lw our_width pt our_point, \
     1/0 title 'benchmark' w l lc exact_color lw exact_width, \
     "k0.1-my.txt" using 1:($4*U) every 1 notitle w l lc exact_color lw exact_width
