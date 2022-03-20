fdac27b28237b80e48cb36f5d0293099f68aba98
Oleg Rogozin <o.a.rogozin@gmail.com>
2022-03-20 10:38:14 +0300

    [moving_heat_source] evaluate temperature gradients

--- reports/14_moving_heat_source/plots/pool.py
+++ reports/14_moving_heat_source/plots/pool.py
@@ -8,11 +8,16 @@ from scipy.special import erf
 from numpy import sqrt, exp, log
 from functools import partial
 
-parser = argparse.ArgumentParser(
-    description='Draw a solid--liquid interface of a melt pool')
+parser = argparse.ArgumentParser(description='Draw a solid--liquid interface of a melt pool')
+
+modes = { 'b': 'boundary', 'g': 'gradient' }
+class ParseMode(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string):
+        setattr(namespace, self.dest, modes[values[0]])
 
 str2pair = lambda s: [float(item) for item in s.split(':')]
 
+parser.add_argument('mode', choices=[*modes.keys(), *modes.values()], action=ParseMode, help='Execution mode')
 parser.add_argument('-N', type=int, default=100, help='number of points along each axis')
 parser.add_argument('-T', '--Tratio', type=float, default=0.05, help='T_M/T_0')
 parser.add_argument('-s', '--figsize', type=str2pair, default='5:4', help='figure size')
@@ -49,6 +54,7 @@ _rho = lambda r,x: r*exp(-x)
 _norm = lambda r,x: sqrt(r**2 + x**2)
 _contour_eq = lambda model: lambda r,x: func[model](r,x) - args.Tratio
 _depth_eq = lambda model: lambda x: (func[model](*x) - args.Tratio, ddx[model](*x))
+_gradT = lambda model: lambda r,x: _norm(ddx[model](r,x), ddr[model](r,x))
 _mesh = lambda a, b, va, vb: (erf(np.linspace(-va, vb, args.N)) + 1)*(b-a)/2 + a
 _latex = lambda text: r'$\mathrm{' + text.replace(' ', r'\ ') + r'}$'
 
@@ -60,6 +66,11 @@ func = {
 ddx = {
     'Levin': lambda r,x: (1-_xi(x))*((2-_xi(x))/_norm(2-_xi(x), _rho(r,x))**3 - _xi(x)/_norm(_xi(x), _rho(r,x))**3),
     'Rosenthal': lambda r,x: -exp(-(x+_norm(r,x))/2)*(x/_norm(r,x)**3 + (1 + x/_norm(r,x))/2/_norm(r,x))
+}
+
+ddr = {
+    'Levin': lambda r,x: _rho(r,x)*(1-_xi(x))**2*(1/_norm(2-_xi(x), _rho(r,x))**3 - 1/_norm(_xi(x), _rho(r,x))**3),
+    'Rosenthal': lambda r,x: -exp(-(x+_norm(r,x))/2)*r/_norm(r,x)**2*(1/_norm(r,x) + 1/2)
 }
 
 if args.debug:
@@ -75,36 +86,61 @@ logN = np.log10(args.N)
 small, large = 10*np.finfo(float).eps, 1e2*log(2/args.Tratio)**(1/3)
 depthAtZeroX = (args.Tratio)**(-1/3)
 lengthApprox = 1.5*log(2/args.Tratio)
-if args.verbose:
+if args.verbose and args.mode == modes['b']:
     print(f'First estimation: length = {lengthApprox:.5g}, depth = {depthAtZeroX:.5g}')
 
-for label in [ 'Levin 2008', 'Rosenthal 1946' ]:
+for n, label in enumerate([ 'Levin 2008', 'Rosenthal 1946' ]):
     model = label.split(' ')[0]
     xmin = root_scalar(partial(_contour_eq(model), 0), bracket=[-large, -small]).root
     xmax = root_scalar(partial(_contour_eq(model), 0), bracket=[small, large]).root
     length = xmax - xmin
     depth, xdepth = root(_depth_eq(model), [depthAtZeroX, 0], method='lm').x
-    if args.verbose:
-        print(f'{label}: {xmin:.5g} < x < {xmax:.5g}, length = {length:.5g}, depth = {depth:.5g}')
     X = _mesh(xmin, xmax, logN+1, logN+1.5)
-    Y = [ root_scalar(_contour_eq(model), args=x, bracket=[small, large]).root for x in X ]
-    plt.plot(X, -np.array(Y), label=_latex(label))
-    plt.plot([xmin, xmax], [0, 0], **Style.point)
-    plt.plot(xdepth, -depth, **Style.point)
-    if label.startswith('R'):
-        plt.annotate('$\hat{x}_\mathrm{min}$', (xmin, 0), **Style.annotate)
-        plt.annotate('$\hat{x}_\mathrm{max}$', (xmax, 0), **Style.annotate)
-        plt.plot([xdepth, 0], [-depth, -depth], **Style.dashed)
-        plt.plot(0, -depth, **Style.point)
-        plt.annotate('$-\hat{d}$', (0, -depth), **Style.annotate)
+    R = np.array([ root_scalar(_contour_eq(model), args=x, bracket=[small, large]).root for x in X ])
+    minmax = np.array([xmin, xmax])
+
+    ### Mode 1: Melt-pool boundary
+    if args.mode == modes['b']:
+        if args.verbose:
+            print(f'{label:>14s}: {xmin:.5g} < x < {xmax:.5g}, length = {length:.5g}, depth = {depth:.5g}')
+        plt.plot(X, -R, label=_latex(label))
+        plt.plot(minmax, np.zeros(2), **Style.point)
+        plt.plot(xdepth, -depth, **Style.point)
+        if label.startswith('R'):
+            plt.annotate('$\hat{x}_\mathrm{min}$', (xmin, 0), **Style.annotate)
+            plt.annotate('$\hat{x}_\mathrm{max}$', (xmax, 0), **Style.annotate)
+            plt.plot([xdepth, 0], [-depth, -depth], **Style.dashed)
+            plt.plot(0, -depth, **Style.point)
+            plt.annotate('$-\hat{d}$', (0, -depth), **Style.annotate)
+
+    ### Mode 2: Temperature gradient along the melt-pool boundary
+    elif args.mode == modes['g']:
+        GradT = _gradT(model)(R,X)
+        gradTdepth = _gradT(model)(depth, xdepth)
+        sol, fus = X < xdepth, X > xdepth
+        X1, GradT1 = np.append(X[sol], xdepth), np.append(GradT[sol], gradTdepth)
+        X2, GradT2 = X[fus], GradT[fus]
+        plt.plot(X1, GradT1, label=_latex(label))
+        plt.plot(X2, GradT2, color=f'C{n}', linestyle='--')
+        plt.plot(minmax, _gradT(model)(np.zeros(2), minmax), **Style.point)
+        plt.plot(xdepth, gradTdepth, **Style.point)
+        if args.verbose:
+            print(f'{label:>14s}: temperature gradient at solidification: ' +
+                f'min = {np.min(GradT1):.5g}, max = {np.max(GradT1):.5g}' +
+                f' at fusion: min = {np.min(GradT2):.5g}, max = {np.max(GradT2):.5g}')
+
+if args.mode == modes['b']:
+    plt.axvline(x=0, **Style.thin)
+    plt.axis('scaled')
+    plt.ylim(top=depth/4)
+    plt.ylabel(r'$\hat{z}$', rotation=0)
+elif args.mode == modes['g']:
+    plt.ylabel(r'$\hat{\nabla}\hat{T}$', rotation=0, ha='right')
 
 plt.axhline(y=0, **Style.thin)
-plt.axvline(x=0, **Style.thin)
-plt.axis('scaled')
-plt.ylim(top=depth/4)
-plt.xlabel('$\hat{x}$')
-plt.ylabel('$\hat{z}$', rotation=0)
+plt.xlabel(r'$\hat{x}$')
 plt.legend()
+
 if args.xrange:
     plt.xlim(args.xrange)
 if args.yrange:
