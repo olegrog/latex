1041dd5029960dbe330a21911989bc3004d5be12
Oleg Rogozin <o.a.rogozin@gmail.com>
2024-08-02 01:21:49 +0300

    [shakhov_model] add two-beam distribution function

--- reports/17_shakhov_model/relaxation/solver.py
+++ reports/17_shakhov_model/relaxation/solver.py
@@ -15,10 +15,12 @@ parser.add_argument('-e', '--end', type=float, default=10, help='total time')
 parser.add_argument('--tau', type=float, default=1, help='relaxation time')
 parser.add_argument('-f', '--function', default='piecewise', help='type of VDF')
 parser.add_argument('-r', '--ratio', type=float, default=2, help='ratio of values in VDF')
-parser.add_argument('-c', '--cut', type=float, default=2, help='cutting radius of VDF')
+parser.add_argument('-i', '--inner', type=float, default=1, help='outer cutting radius of VDF')
+parser.add_argument('-o', '--outer', type=float, default=2, help='outer cutting radius of VDF')
 parser.add_argument('-R', '--radius', type=float, default=5, help='radius of the velocity grid')
 parser.add_argument('-g', '--grid', default='uniform', metavar='uniform|hermite|polynomial|geometric', help='type of the grid')
 parser.add_argument('-p', '--plot', type=int, default=10, help='plot every <int> steps')
+parser.add_argument('-l', '--log', action='store_true', help='use log axes')
 parser.add_argument('--gratio', type=float, default=1.15, help='ratio for the geometric grid')
 parser.add_argument('--w-min', type=float, default=0.1, help='minimum weight for the polynomial grid')
 parser.add_argument('--poly', type=float, default=2, help='power for the polynomial grid')
@@ -35,16 +37,19 @@ sqr = lambda x: x*x
 _c = lambda vel: xi - vel
 _cc = lambda vel: sqr(xi - vel)
 
-def calc_macro(f):
+def calc_macro(f, time = -1):
     rho = np.sum(f)
     speed = np.sum(f*xi)/rho
     c, cc = _c(speed), _cc(speed)
     temp = 2*np.sum(f*cc)/rho
     qflow = np.sum(f*c*cc)
-    hfunc = np.sum(f*log(f/w))
+    pos, neg = f>0, f<0
+    hfunc = np.sum(f[pos]*log((f/w)[pos]))
+    if time > -1 and np.count_nonzero(neg) > 0:
+        print(f'Negative VDF at {time}: xi = {xi[neg]}, f = {f[neg]}')
     return rho, speed, temp, qflow, hfunc
 
-def maxwell():
+def maxwell(macro):
     ones = np.ones_like(w)
     maxw5 = lambda m: w * m[0] * (np.pi*m[2])**(-fixed.D/2) * np.exp(-_cc(m[1]) / m[2])
     maxw5_jac = lambda m: np.vstack((
@@ -54,7 +59,6 @@ def maxwell():
     ))
 
     psi = np.vstack(( ones, xi, sqr(xi) ))
-    macro = np.array([ rho, speed, temp ])
     moments = rho*np.array([ 1, speed, fixed.D*temp/2 ])
     vdf = maxw5
     vdf_jac = lambda m: np.einsum('i,li->li', maxw5(m), maxw5_jac(m))
@@ -66,21 +70,35 @@ def maxwell():
     )
     return vdf(sol.x)
 
+def grad13(macro):
+    qc = lambda m: qflow * _c(m[1])
+    cc_5T = lambda m: _cc(m[1]) / m[2] / (fixed.D+2)
+    g_qflow = lambda m: qc(m) * (cc_5T(m) - 1)
+    vdf = lambda m: maxwell(macro) * (1 + g_qflow(m)) / m[0] / m[2]**2
+    return vdf(macro)
+
 def initial_vdf():
     f = np.zeros_like(xi)
-    if args.function == 'piecewise':
-        c = args.cut
-        if (c > args.radius or c/args.ratio > args.radius):
-            raise ValueError('Cutting radius is too big!')
-        neg, pos = (-c < xi) * (xi < 0), (0 < xi) * (xi < c/sqrt(args.ratio))
-        ratio = -np.sum((xi*w)[neg])/np.sum((xi*w)[pos])
-        mass = np.sum(w[neg]) + ratio*np.sum(w[pos])
-        f[neg] = 1/mass
-        f[pos] = ratio/mass
-        if args.verbose:
-            print(f'VDF: left = {f[neg][0]:.3g}, right = {f[pos][0]:.3g}, ratio = {ratio:.3g}')
-    else:
-        raise ValueError('Function type is undefined!')
+    match args.function:
+        case 'piecewise':
+            i, o = args.inner, args.outer
+            ratio, R = args.ratio, args.radius
+            if (i >= o):
+                raise ValueError('Inner radius is larger than the outer one!')
+            if (o > R or o/sqrt(ratio) > R):
+                raise ValueError('Cutting radius is too big!')
+            neg, pos = (-o < xi) * (xi < -i), (i/sqrt(ratio) < xi) * (xi < o/sqrt(ratio))
+            ratio = -np.sum((xi*w)[neg])/np.sum((xi*w)[pos])
+            mass = np.sum(w[neg]) + ratio*np.sum(w[pos])
+            f[neg] = 1/mass
+            f[pos] = ratio/mass
+            if args.verbose:
+                print(f'VDF: left = {f[neg][0]:.3g}, right = {f[pos][0]:.3g}, ratio = {ratio:.3g}')
+        case 'grad13':
+            macro = np.array([1, 0, 1])
+            f = grad13(macro)
+        case _:
+            raise ValueError('Function type is undefined!')
     return f*w
 
 def print_macro(f):
@@ -102,21 +120,27 @@ grid = getattr(vgrid, args.grid.capitalize())(args.radius, args.N, **vgrid_param
 xi, w = grid.x, grid.w
 f_0 = initial_vdf()
 rho, speed, temp, qflow0, h0 = calc_macro(f_0)
-f_M = maxwell()
+macro = np.array([ rho, speed, temp ])
+f_M = maxwell(macro)
+#f_0 = np.maximum(f_0 - f_M, 0)
+#rho, speed, temp, qflow0, h0 = calc_macro(f_0)
+macro = np.array([ rho, speed, temp ])
+#f_M = maxwell(macro)
 _, _, _, _, h_M = calc_macro(f_M)
 Q1, Q2, H, X = [], [], [], []
 
+### 1. Plot VDF
 for i in range(int(args.end // args.timestep)):
     time = i*args.timestep
     if args.verbose:
         print(f'# Iteration = {i}, time = {time:.5g}')
-    e1, e23 = exp(-time/args.tau), exp(-2/fixed.D*time/args.tau)
+    e1, e23 = exp(-time/args.tau), exp(-1/fixed.D*time/args.tau)
     q = qflow0*e23
     A = (1-fixed.Pr)/5/pi**(fixed.D/2)*4*q/rho/temp**2*_c(speed)*(_cc(speed)/temp - 5/2)
     f = f_0*e1 + f_M*(1 - e1 + fixed.D*A*(1 - e23))
 
     Q1.append(q)
-    rho, speed, temp, qflow, hfunc = calc_macro(f)
+    rho, speed, temp, qflow, hfunc = calc_macro(f, time)
     Q2.append(qflow)
     H.append(hfunc)
     X.append(time)
@@ -135,6 +159,7 @@ for i in range(int(args.end // args.timestep)):
         else:
             plt.show()
 
+### 2. Plot heat flux
 X = np.array(X); H = np.array(H)
 Q1 = np.array(Q1); Q2 = np.array(Q2)
 plt.plot(X, -Q1, label='Q1')
@@ -142,12 +167,23 @@ plt.plot(X, -Q2, label='Q2')
 plt.title('Heat flux')
 plt.legend()
 plt.semilogy()
-plt.show()
+if args.pdf:
+    plt.savefig(f'heat_flux.pdf', bbox_inches='tight')
+    plt.close()
+else:
+    plt.show()
 
+### 3. Plot H-function
+plt.margins(x=0)
+color1 = plt.rcParams['axes.prop_cycle'].by_key()['color'][1]
+plt.axhline(y=h_M, ls='--', color=color1)
 plt.plot(X, H)
-plt.axhline(y=h_M, lw=.5, c='k', ls=':')
-plt.semilogx()
+if args.log:
+    plt.semilogx()
+else:
+    plt.axvline(lw=.5, c='k', ls='-')
 
+print('H-function always decreases:', np.all(H[1:] < H[:-1]))
 if args.pdf:
     plt.savefig('solution.pdf', bbox_inches='tight')
 else:
